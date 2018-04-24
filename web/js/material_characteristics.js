ecd18b6beac0b505b9bcfbd84403d2661e26e547
Clément BRUN <clement.brun.alvx@gmail.com>
2018-04-24 12:04:49 +0200

    Ajout js et correction anglais dans les interfaces

--- web/js/material_characteristics.js
+++ web/js/material_characteristics.js
@@ -53,3 +53,154 @@ function startSession()
 }
 
 $(document).ready(startSession);
+
+$( function clientFileNumber() {
+    var availableNumber = [
+    	'1111',
+    	'2222',
+    	'1234',
+    	'156'
+    ];
+    $( "#client_file_number" ).autocomplete({
+      source: availableNumber
+    });
+  } );
+
+$( function partNumber() {
+    var availablePartNumber = [
+    	'10101-1250',
+    	'10101-1729',
+    	'10101-3981',
+    ];
+    $( "#part_number" ).autocomplete({
+      source: availablePartNumber
+    });
+  } );
+
+$( function materialGrade() {
+    var grade = [
+    	'16CrNi4SPb',
+    	'100Cr6',
+    	'11SMn30 (s250)',
+    	'11SMn30Pb (s250Pb)',
+    	'11SMn30Pb+',
+    	'11SMn37 (s300)',
+    	'11SMn37Pb',
+    	'15SMn13',
+    	'16MnCr5',
+    	'16MnCrS5',
+    	'20MnCr5',
+    	'20NiCrMo2-2',
+    	'34CrMo4',
+    	'36SMnPb14',
+    	'38SMn28',
+    	'40SMnSi30',
+    	'44SMn28',
+    	'51CrV4',
+    	'C22',
+    	'C35E (+H)',
+    	'C40',
+    	'C45Pb',
+    	'C60E',
+
+    ];
+    $( "#material_grade" ).autocomplete({
+      source: grade
+    });
+  } );
+
+$( function materialPricePerTon() {
+    var pricePerTon = [
+    	'1111',
+    	'2222',
+    	'1234',
+    	'156'
+    ];
+    $( "#material_price_per_ton" ).autocomplete({
+      source: pricePerTon
+    });
+  } );
+
+$( function unitarianPartPrice() {
+    var partPrice = [
+    	'1111',
+    	'2222',
+    	'1234',
+    	'156'
+    ];
+    $( "#part_price" ).autocomplete({
+      source: partPrice
+    });
+  } );
+
+$( function materialTensileStregth() {
+    var tensileStregth = [
+    	'1111',
+    	'2222',
+    	'1234',
+    	'156'
+    ];
+    $( "#material_tensile_stregth" ).autocomplete({
+      source: tensileStregth
+    });
+  } );
+
+$( function materialProofStress() {
+    var proofStress = [
+    	'1111',
+    	'2222',
+    	'1234',
+    	'156'
+    ];
+    $( "#material_proof_stress" ).autocomplete({
+      source: proofStress
+    });
+  } );
+
+$( function materialHardness() {
+    var hardness = [
+    	'1111',
+    	'2222',
+    	'1234',
+    	'156'
+    ];
+    $( "#material_hardness" ).autocomplete({
+      source: hardness
+    });
+  } );
+
+$( function materialElongation() {
+    var elongation = [
+    	'1111',
+    	'2222',
+    	'1234',
+    	'156'
+    ];
+    $( "#material_elongation" ).autocomplete({
+      source: elongation
+    });
+  } );
+
+$( function materialWeldability() {
+    var weldability = [
+    	'1111',
+    	'2222',
+    	'1234',
+    	'156'
+    ];
+    $( "#weldability" ).autocomplete({
+      source: weldability
+    });
+  } );
+
+$( function heatTreatability() {
+    var treatability = [
+    	'1111',
+    	'2222',
+    	'1234',
+    	'156'
+    ];
+    $( "#heat_treatability" ).autocomplete({
+      source: treatability
+    });
+  } );
