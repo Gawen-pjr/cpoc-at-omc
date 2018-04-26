6719391214b34098253df6591ee4cad79a9b4a87
Pierre-Louis JAEGER <pjr@iknova.com>
2018-04-26 19:32:33 +0200

    Ajout d'un fichier JS dédié à la gestion de l'API Kvoweb

---
+++ web/js/kvoweb.js
@@ -0,0 +1,125 @@
+// Liaison entre le formulaire et Kadviser
+
+var _kvoweb =
+{
+    KVOWEB_BASE_URL : 'https://alpenbox.kad-office.com/kvoweb/api/v1.0',
+    USER : 'toto',
+    PASS : 'titi',
+
+    userToken : undefined,
+    session : undefined,
+
+    apiCall : function(endpoint, method, data, callback)
+    {
+        $.ajax(_kvoweb.KVOWEB_BASE_URL + endpoint,
+        {
+            method : method,
+            headers :
+            {
+                'X-User-Token' : _kvoweb.userToken
+            },
+            data : data
+        }).done(callback);
+    },
+
+    apiGet : function(endpoint, callback)
+    {
+        _kvoweb.apiCall(endpoint, 'GET', undefined, callback);
+    },
+
+    apiPost : function(endpoint, data, callback)
+    {
+        _kvoweb.apiCall(endpoint, 'POST', data, callback);
+    },
+
+    apiDelete : function(endpoint, callback)
+    {
+        _kvoweb.apiCall(endpoint, 'DELETE', undefined, callback);
+    },
+
+    apiPatch : function(endpoint, data, callback)
+    {
+        _kvoweb.apiCall(endpoint, 'PATCH', data, callback);
+    },
+
+    apiPut : function(endpoint, data, callback)
+    {
+        _kvoweb.apiCall(endpoint, 'PUT', data, callback);
+    },
+
+    login : function(callback)
+    {
+        var data =
+        {
+            login : _kvoweb.USER,
+            password : _kvoweb.PASS
+        };
+
+        _kvoweb.apiPost('/user/login', data, function(data)
+        {
+            console.debug('OMC', 'User token: ' + data.token);
+            _kvoweb.userToken = data.token;
+            callback();
+        });
+    },
+
+    init : function()
+    {
+        _kvoweb.login(function()
+        {
+            _kvoweb.session = JSON.parse(window.localStorage.getItem("kvoweb.session"));
+
+            if (_kvoweb.session)
+            {
+                console.info('OMC', 'Retrieved session #' + _kvoweb.session.id);
+            }
+            else
+            {
+                _kvoweb.createAndActivateSession();
+            }
+        });
+    },
+
+    saveSession : function()
+    {
+    // TODO à implémenter
+    },
+
+    deleteOldSessions : function()
+    {
+        _kvoweb.apiDelete('/session');
+    },
+
+    restartSession : function()
+    {
+        if (_kvoweb.session)
+        {
+            _kvoweb.apiDelete('/session/' + _kvoweb.session.id, _kvoweb.createAndActivateSession);
+        }
+        else
+        {
+            _kvoweb.createAndActivateSession()
+        }
+    },
+
+    createAndActivateSession : function()
+    {
+        var data =
+        {
+            name : 'POC AT-OMC',
+            amiId : 1
+        };
+
+        _kvoweb.apiPost('/session', data, function(data)
+        {
+            _kvoweb.apiPost('/session/' + data.id + '/activate', undefined, function(data)
+            {
+                _kvoweb.session = data;
+                window.localStorage.setItem("kvoweb.session", JSON.stringify(data));
+                console.info('OMC', 'Activated session #' + data.id);
+            });
+        });
+    },
+};
+
+window.kvoweb = _kvoweb;
