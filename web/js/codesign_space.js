125da8f5eecbeb640a504b1bbddc43c3d95ca671
Pierre-Louis JAEGER <pjr@iknova.com>
2018-05-01 17:39:54 +0200

    Finalisation du formulaire codesign_space avec mise en cache des données

--- web/js/codesign_space.js
+++ web/js/codesign_space.js
@@ -1,27 +1,31 @@
 
-kvoweb.init();
+omc.init();
 
-var sessionGrade = window.localStorage.getItem("omc.referenceGrade");
-var currentGrade = sessionGrade ? JSON.parse(sessionGrade) : undefined;
-
-jQuery(function($)
-{
-    $('.slidebar').each(function()
+jQuery($ => {
+    if(!omc.userMaterial)
     {
-        var $container = $(this);
-        
+        $('#price-index-section').css('display','none');
+    }
+
+    var currentState = omc.toleranceIntervals || {};
+
+    // Configuration des slidebars
+    $('.slidebar').each((index, container) => {
+        var $container = $(container);
+        var $tooltipImage = $('img.info_logo', container);
+        var $textInput = $('input', container);
+        var $slider = $('div.slider-range', container);
+        var $valueBar = $('div.value-bar', container);
+        var $valueHint = $('div.value-hint', container);
+
         var characteristic = $container.attr('data-characteristic');
         var min = Number($container.attr('data-min')) || 0;
         var max = Number($container.attr('data-max')) || 10000;
         var unit = $container.attr('data-unit') || '';
         var multiplier = Number($container.attr('data-multiplier')) || 1;
+        var delta = 0.1 * (max - min);
         
-        var $tooltipImage = $('img.info_logo', $container);
-        var $textInput = $('input', $container);
-        var $slider = $('div.slider-range', $container);
-        var $valueBar = $('div.value-bar', $container);
-        var $valueHint = $('div.value-hint', $container);
-        
+
         $tooltipImage.attr('title',$tooltipImage.attr('title') + ' [' + min + unit + '; ' + max + unit + ']');
         
         function updateText(v1, v2)
@@ -30,25 +34,42 @@ jQuery(function($)
         }
         
         var val = Number($valueHint.text());
-        if(currentGrade && currentGrade.characteristics[characteristic])
+        if(omc.userMaterial && omc.userMaterial.characteristics[characteristic])
         {
-            val = currentGrade.characteristics[characteristic] * multiplier;
-        }
-        if(!val)
-        {
-            val = (min + max) / 2;
+            val = omc.userMaterial.characteristics[characteristic] * multiplier;
         }
 
-        console.debug('OMC',characteristic + ' = ' + val);
+        if(val)
+        {
+            console.debug('OMC',characteristic + ' = ' + val);
 
-        var delta = 0.1 * (max - min);
-        var v1Init = Math.floor(Math.max(min, val - delta));
-        var v2Init = Math.ceil(Math.min(max, val + delta));
+            var v1Init = Math.floor(Math.max(min, val - delta));
+            var v2Init = Math.ceil(Math.min(max, val + delta));
 
-        var relativePosition = (val - min) / (max - min) * 100;
-        $valueBar.css('left',relativePosition + '%');
-        $valueHint.css('left',relativePosition + '%');
-        $valueHint.text(val + unit);
+            var relativePosition = (val - min) / (max - min) * 100;
+            $valueBar.css('left',relativePosition + '%');
+            $valueHint.css('left',relativePosition + '%');
+            $valueHint.text(val.toFixed(0) + unit);
+        }
+        else
+        {
+            var middle = (min + max) / 2;
+            var v1Init = Math.floor(middle - delta);
+            var v2Init = Math.ceil(middle + delta);
+            $valueBar.css('display','none');
+            $valueHint.css('display','none');
+        }
+        
+        if(currentState && currentState[characteristic])
+        {
+            v1Init = Math.min(val || max,currentState[characteristic][0]);
+            v2Init = Math.max(val || min,currentState[characteristic][1]);
+        }
+        else
+        {
+            currentState[characteristic] = [v1Init, v2Init];
+        }
+
         updateText(v1Init, v2Init);
 
         $slider.slider(
@@ -58,20 +79,19 @@ jQuery(function($)
             min : min,
             max : max,
             values : [ v1Init, v2Init ],
-            slide : function(event, ui)
-            {
+            slide : (event, ui) => {
                 var v1 = ui.values[0];
                 var v2 = ui.values[1];
                 var centralValueReached = false;
                 
-                if(v1 > val)
+                if(val && (v1 > val))
                 {
                     v1 = val;
                     $slider.slider("values", 0, v1);
                     centralValueReached = true;
                 }
 
-                if(v2 < val)
+                if(val && (v2 < val))
                 {
                     v2 = val;
                     $slider.slider("values", 1, v2);
@@ -80,21 +100,53 @@ jQuery(function($)
                 
                 updateText(v1, v2);
                 return !centralValueReached;
-            }
+            },
+            change: (event, ui) => {
+                currentState[characteristic] = ui.values;
+                omc.saveToleranceIntervals(currentState);
+            },
         });
     });
 
-    //Radiobox
-    $("input[type='radio']").checkboxradio(); // inupt.classe_des_radioboutons
-    $("fieldset").controlgroup();
+    // Configuration des radio buttons
+    $('.radiobutton').each((index, container) => {
+        var $fieldset = $('fieldset', container);
+        var $inputs = $('input[type="radio"]', container);
+        var $labels = $('label', container);
+        
+        var characteristic = $(container).attr('data-characteristic');
 
-    $('#calculation_button').button().click(function()
-    {
-        window.location = 'mire_ramo.html';
-    });
+        if(currentState[characteristic])
+        {
+            $('input[value="' + currentState[characteristic][1] + '"]',container).prop("checked", true);
+        }
+        else if(omc.userMaterial)
+        {
+            var val = omc.userMaterial.characteristics[characteristic];
+            $('input[value="' + val + '"]',container).prop("checked", true);
+            currentState[characteristic] = [1, val];
+        }
 
-    $('#return_button').button().click(function()
-    {
-        window.location = 'material_characteristics.html';
+        omc.withMaterialDB(db => $labels.each((index,label) => {
+            var $l = $(label);
+            if(!$l.text())
+            {
+                $l.text(db.messages[$l.attr('for')]);
+            }
+        }));
+        $fieldset.controlgroup();
+        
+        $inputs.change(event => {
+            if(event.target.checked)
+            {
+                var $input = $(event.target);
+                currentState[characteristic] = [1,Number($input.attr('value'))];
+                omc.saveToleranceIntervals(currentState);
+            }
+        })
     });
+    
+    // Configuration des boutons de navigation
+    $('#calculation_button').button().click(() => window.location = 'mire_ramo.html');
+    $('#return_button').button().click(() => window.location = 'material_characteristics.html');
 });
