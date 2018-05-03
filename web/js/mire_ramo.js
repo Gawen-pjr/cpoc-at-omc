f680f58140d9cc22826df252a94b35088ff51a1e
Pierre-Louis JAEGER <pjr@iknova.com>
2018-05-04 00:22:38 +0200

    Affichage des mires dans la cible RAMO à partir des appels Kadviser

--- web/js/mire_ramo.js
+++ web/js/mire_ramo.js
@@ -1,26 +1,132 @@
 
+var KV_ATTRIBUTES = [ 'Module_young', 'Resistance_traction', 'Limite_elastique2', 'Durete', 'Allongement', 'Cout_operation', 'Soudabilite', 'Traitement_surface', 'Indice_outillage', 'Usinabilite', 'Prix_tonne' ];
+var JS_ATTRIBUTES = [ 'e', 'rm', 'rp', 'hb', 'a', 'pi', 's', 'ts', 'iTool', 'u', 'pricePerTon' ];
+
+function setAttributeInterval(index, object, characteristics, callback)
+{
+    if(index >= JS_ATTRIBUTES.length)
+    {
+        callback();
+        return;
+    }
+
+    var interval = characteristics[JS_ATTRIBUTES[index]];
+    if(typeof interval === "undefined")
+    {
+        setAttributeInterval(index + 1, object, characteristics, callback);
+        return;
+    }
+    
+    if(typeof interval !== "object")
+    {
+        interval = [interval];
+    }
+
+    var kvValue = ((interval.length > 1) && (interval[0] != interval[1])) ? ('[ ' + interval[0] + ' ; ' + interval[1] + ' ]') : interval[0];
+    kvoweb.setAttributeValue(object, KV_ATTRIBUTES[index], kvValue, () => setAttributeInterval(index + 1, object, characteristics, callback));
+}
+
+function sendMaterialCharacteristics(object, characteristics, callback)
+{
+    setAttributeInterval(0, object, characteristics, callback);
+}
+
+function sendToleranceIntervals(callback)
+{
+    var intervals = omc.toleranceIntervals || omc.defaultIntervals;
+    if(!intervals)
+    {
+        return;
+    }
+    sendMaterialCharacteristics('mat', intervals, callback);
+}
+
+function prepareM0Grade(callback)
+{
+    if(!omc.userMaterial)
+    {
+        return;
+    }
+    sendMaterialCharacteristics('mat_m0', omc.userMaterial.characteristics, callback);
+}
+
+function sendMatchingGrade(grades, index, callback)
+{
+    if(index >= grades.length)
+    {
+        return;
+    }
+
+    kvoweb.setAttributeValue('mat', 'Nuance', grades[index], () => {
+
+        sendMatchingGrade(grades, index + 1, callback);
+
+        var kvMat = kvoweb.session.activeObjects.mat;
+        var mat = {
+            characteristics: {},
+            name: grades[index],
+        };
+
+        for(var i = 0; i < KV_ATTRIBUTES.length; i++)
+        {
+            mat.characteristics[JS_ATTRIBUTES[i]] = kvMat.attributes[KV_ATTRIBUTES[i]].value;
+        }
+
+        var intervals = omc.toleranceIntervals || omc.defaultIntervals;
+        if(!intervals || ((mat.characteristics.pi >= intervals.pi[0]) && (mat.characteristics.pi <= intervals.pi[1])))
+        {
+                callback(mat);
+        }
+    });
+}
+
+function sendMatchingGrades(grades, callback)
+{
+    sendMatchingGrade(grades, 0, callback);
+}
+
+function processFilteredGrades()
+{
+    var grades = kvoweb.session.activeObjects.mat.attributes.Nuance.value.split('\n').filter(g => (g != 'Inconnue') && (g != omc.userMaterial.name));
+    console.debug('OMC', grades);
+    
+    kvoweb.restartSession();
+    kvoweb.withSession(() => prepareM0Grade(() => sendMatchingGrades(grades,displayMatchingMaterial)));
+}
+
+function displayMatchingMaterial(material)
+{
+    console.debug('OMC', material);
+
+    var x0 = omc.userMaterial.characteristics.rm;
+    var xAmp = Math.max(x0 - 360.0, 1200.0 - x0);
+
+    var y0 = omc.userMaterial.characteristics.pricePerTon;
+    var yAmp = Math.max(y0 - 750.0, 4600.0 - y0);
+
+    var intervals = omc.toleranceIntervals || omc.defaultIntervals;
+    if(intervals)
+    {
+        xAmp = Math.max(x0 - intervals.rm[0], intervals.rm[1] - x0);
+    }
+
+    var x = Number(material.characteristics.rm); // TODO pouvoir choisir l'axe y
+    var y = Number(material.characteristics.pricePerTon);
+    var xs = (x - x0) * (350.0 / xAmp);
+    var ys = (y - y0) * (350.0 / yAmp);
+    var title = material.name + ' (Rm = ' + x.toFixed(0) + ' Mpa, Price per ton = ' + y.toFixed(0) + ', Price index = ' + Number(material.characteristics.pi).toFixed(0) + ')';
+    mireFactory.create('#axe_abscisses', 'mire_' + material.name, 350 + xs, -5 - ys,'#008800').attr('title', title);
+}
+
+window.localStorage.removeItem("kvoweb.session");
+omc.init();
 kvoweb.init();
 
-$( function() 
-{
-    $( "#performance_index_select" ).selectmenu();
-});
+kvoweb.withSession(() => prepareM0Grade(() => sendToleranceIntervals(processFilteredGrades)));
 
-jQuery(function($)
-{
-    $('#back_button').button().click(function()
-    {
-        window.location = 'codesign_space.html';
-    });
-
-    $('#print_button').button().click(function()
-    {
-        window.location = 'material_characteristics.html';
-    });
-
-    $('#homepage_button').button().click(function()
-    {
-        window.location = 'material_characteristics.html';
-    });
-
+jQuery($ => {
+    $( "#performance_index_select" ).selectmenu().attr('disabled',true);
+    $('#back_button').button().click(() => window.location = 'codesign_space.html');
+    $('#print_button').button().click(() => window.location = 'material_characteristics.html');
+    $('#homepage_button').button().click(() => window.location = 'material_characteristics.html');
 });
