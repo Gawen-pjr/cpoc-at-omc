741e4f807f80cdd1635536c74a589bd0b5b16522
Antonin LEYVAL <all@iknova.com>
2018-08-14 15:50:12 +0200

    Mise en place du submit

--- tools/js/index.js
+++ tools/js/index.js
@@ -3,6 +3,4 @@ jQuery($ => {
     $.getJSON("poc.json", meta =>
         $('#versionning').text("v" + meta.version + " du " + meta.release_date)
     );
-
-    // $('#upload_button').click(() => console.log("click"));
 });
