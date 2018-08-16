8b5169fb20ab526b74a90a46dc32ef0e123d3559
Antonin LEYVAL <all@iknova.com>
2018-08-16 14:29:06 +0200

    Amélioration de l'index pour tenir compte du choix de bdd

--- tools/js/index.js
+++ tools/js/index.js
@@ -1,6 +1,16 @@
+var chosenBdd = "";
+
 jQuery($ => {
 
     $.getJSON("poc.json", meta =>
         $('#versionning').text("v" + meta.version + " du " + meta.release_date)
     );
+
+    $('#bdd_select').blur(() =>
+    {
+    	chosenBdd = $('#bdd_select option:selected').val();
+    	var form = document.getElementById('submit_form');
+		form.action = "https://alpenbox.kad-office.com/cxf/omc/material-db/" + chosenBdd + "/csv";
+    	console.debug(form.action);
+    });
 });
