407d4567aa659e199bae04bfb08772c36bfed07e
Antonin LEYVAL <all@iknova.com>
2018-08-20 13:19:20 +0200

    Mise en place de la persistance du selecteur de bdd

--- tools/js/index.js
+++ tools/js/index.js
@@ -1,4 +1,4 @@
-var chosenBdd = "";
+var chosenBdd = $('#bdd_select option:selected').val();
 
 jQuery($ => {
 
@@ -6,11 +6,29 @@ jQuery($ => {
         $('#versionning').text("v" + meta.version + " du " + meta.release_date)
     );
 
+    formAction = localStorage.getItem("formAction");
+
+    if (formAction)
+    {
+        var currentBdd = localStorage.getItem("chosenBdd");
+        $('#bdd_select option[value=' + currentBdd + ']').attr('selected','selected');
+        var form = document.getElementById('submit_form');
+        form.action = formAction;
+    }
+    else
+    {
+        var form = document.getElementById('submit_form');
+        form.action = "https://alpenbox.kad-office.com/cxf/omc/material-db/bdd-steels/csv";
+    }
+
     $('#bdd_select').blur(() =>
     {
     	chosenBdd = $('#bdd_select option:selected').val();
+        localStorage.setItem("chosenBdd", chosenBdd);
+
     	var form = document.getElementById('submit_form');
 		form.action = "https://alpenbox.kad-office.com/cxf/omc/material-db/" + chosenBdd + "/csv";
+        localStorage.setItem("formAction", form.action);
         omc.MATERIAL_DB_URL = form.action;
     });
 });
